8afa3d05e451aacf7d115922beafa97fb90738ea
erikrause <erkimkamilov@gmail.com>
2019-11-09 04:55:48 +0500

    getData implementation

--- Classes/Record.cs
+++ Classes/Record.cs
@@ -1,16 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Collections.Generic;
 
 namespace dnc_csharp.Classes
 {
-    public abstract class Record
+    public abstract class Record : INotifyPropertyChanged
     {
-        private string Message;
+        private byte[] Message;
 
-        private string GetData(byte[] start, byte[] end)
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] String propertyName = "")
         {
-            return "Not Implementation";
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));      // Упрощенный делегат (?.Invoke вместо условия if).
+        }
+
+        // Get data from message by bits flags.
+        private byte[] GetData(int startBit, int numberOfBits)
+        {
+            var byteList = new List<byte>();
+            int numberOfBytes = numberOfBits / 8;
+            int remainOfBits = numberOfBits % 8;
+
+            int i;
+            for (i = 0; i < numberOfBytes; i++)
+            {
+                byteList.Add(Message[i]);
+            }
+            if (remainOfBits != 0)
+            {
+                byte mask = (byte)(2 ^ remainOfBits);
+                byte lastBits = (byte)(Message[i] & mask);  // i + 1?
+                byteList.Add(lastBits);
+            }
+
+            return byteList.ToArray();
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -10,27 +10,17 @@ namespace dnc_csharp
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Hello World!");
-            //byte[] ba = Encoding.Default.GetBytes("Hello world!!!");
-            //var hexString = BitConverter.ToString(ba);
-            //hexString = hexString.Replace("-", "");
             Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
             string message = "BB AA 01 00 00 01 00 00 00 00 00 00 07 65 78 61 6d 70 6c 65 03 63 6f 6d 00 00 01 00 01";
-            message = message.Replace(" ", "").Replace("\n", "");
-            string address = "8.8.8.8";
-            int port = 53;
-            //server_address = (address, port);
-            //socket.Connect(address, port);
+            string dnsAddress = "8.8.8.8";
+            int dnsPort = 53;
             byte[] msg = ToByteArray(message);
 
-            IPHostEntry ipHost = Dns.GetHostEntry("8.8.8.8");
+            IPHostEntry ipHost = Dns.GetHostEntry(dnsAddress);
             IPAddress ipAddr = ipHost.AddressList[0];       // Изменил адрес на IPv4
-            // В новых версиях Framework следует использовать
-            // IPAddress ipAddr = ipHost.AddressList[1];
-            EndPoint ipEndPoint = new IPEndPoint(ipAddr, 53);
+            EndPoint ipEndPoint = new IPEndPoint(ipAddr, dnsPort);
             socket.SendTo(msg, ipEndPoint);
-            //socket.Send(msg);
-            //byte response = socket.Receive(4096);
+
             byte[] bytes = new byte[4096];
             int bytesRec = socket.Receive(bytes);
             string response = Encoding.UTF8.GetString(bytes, 0, bytesRec);
@@ -40,6 +30,7 @@ namespace dnc_csharp
 
         public static byte[] ToByteArray(string hex)
         {
+            hex = hex.Replace(" ", "").Replace("\n", "");
             return Enumerable.Range(0, hex.Length)
                              .Where(x => x % 2 == 0)
                              .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
